85d1eec883dce595906344d3f80e9b5017c5e8bc
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-03 22:05:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma."){
-    return "I love you, too."
-  }
-  else if (string === string.toUpperCase() ){
+  if (string === string.toUpperCase() ){
     return "YES INDEED!"
   }
   else {
+    if (string === "I love you, Grandma") {
+      return "I love you, too."
+    }
+    else {
     return "I can't hear you!"
+    }
   }
   
 }
