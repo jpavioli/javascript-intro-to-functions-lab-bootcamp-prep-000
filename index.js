722b4c33a9b4c8e9a2affced102d0479fc480f11
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-03 21:51:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase() ){
+    return console.log("YES INDEED!")
+  }
+  else {
+    return console.log("I can't hear you!")
+  }
+  
+}
